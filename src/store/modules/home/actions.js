b3c3b120b8203b855359f368e8a5c0aa21c20ce1
lz82 <nickylau82@gmail.com>
2020-05-11 00:58:16 +0800

    :construction: init homepage action

---
+++ src/store/modules/home/actions.js
@@ -0,0 +1,26 @@
+export const actionTypes = {
+  SET_STATION_FROM: 'home/set_station_from',
+  SET_STATION_TO: 'home/set_station_to',
+  SWITCH_STATION: 'home/switch_station',
+  SHOW_CITY_SELECT: 'home/show_city_select',
+  HIDE_CITY_SELECT: 'home/hide_city_select',
+  SET_CURRENT_CITY_SIDE: 'home/set_current_city_side',
+  SET_DEPARTURE_DATE: 'home/set_departure_date',
+  TOGGLE_HIGH_SPEED_OPT: 'home/toggle_high_speed_opt'
+};
+
+export const actionCreators = {
+  setStationFrom: (from) => {
+    return {
+      type: actionTypes.SET_STATION_FROM,
+      payload: from
+    };
+  },
+
+  setStationTo: (to) => {
+    return {
+      type: actionTypes.SET_STATION_TO,
+      payload: to
+    };
+  }
+};
