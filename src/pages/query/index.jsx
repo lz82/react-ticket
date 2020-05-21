c9242de7b31e4dda986dc07375eb7c68c6c89f7e
lz82 <nickylau82@gmail.com>
2020-05-21 23:16:55 +0800

    :construction: add urijs lib

--- src/pages/query/index.jsx
+++ src/pages/query/index.jsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import URI from 'urijs';
 
-export default function Query() {
-  return <div>车票查询</div>;
+import Navbar from '@/components/navbar';
+
+import css from './index.module.less';
+
+export default function Query(props) {
+  useEffect(() => {
+    const { from, to, date, highSpeed } = URI.parseQuery(props.location.search);
+    console.log(from, to, date, highSpeed);
+  }, []);
+
+  const onBack = useCallback(() => {
+    props.history.goBack();
+  }, []);
+
+  return (
+    <div className={css['query-wrapper']}>
+      <Navbar title="查询" onBack={onBack} />
+    </div>
+  );
 }
