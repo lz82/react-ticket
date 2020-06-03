01c63e0f6fa33310520ddc1218af6f3a7ada072a
lz82 <nickylau82@gmail.com>
2020-06-04 00:21:44 +0800

    :constructiion: fix eslint error

--- src/pages/ticket/components/candidate/index.jsx
+++ src/pages/ticket/components/candidate/index.jsx
@@ -4,5 +4,6 @@ import css from './index.module.less';
 
 export default function Candidate(props) {
   const { candidates } = props;
+  console.log(candidates);
   return <div className={css['candidate-wrapper']}>candidate</div>;
 }
