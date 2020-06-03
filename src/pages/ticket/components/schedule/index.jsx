6d48fcfb2d014a5a1c9710c63e3bd56be12bd950
lz82 <nickylau82@gmail.com>
2020-06-03 23:40:36 +0800

    :construction: complete schedule

--- src/pages/ticket/components/schedule/index.jsx
+++ src/pages/ticket/components/schedule/index.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import { commonApi } from '@/services';
 
+import css from './index.module.less';
+
 export default function Schedule(props) {
   const [scheduleList, setScheduleList] = useState([]);
 
@@ -19,16 +21,34 @@ export default function Schedule(props) {
   }, []);
 
   return (
-    <div>
-      <div className="dialog">
+    <div className={css['schedule-wrapper']}>
+      <div className={css['dialog']}>
         <h1>列车时刻表</h1>
-        <div className="head">
-          <span className="station">车站</span>
-          <span className="deptime">到达</span>
-          <span className="arrtime">发车</span>
-          <span className="stoptime">停留时间</span>
+        <div className={css['head']}>
+          <span className={css['station']}>车站</span>
+          <span className={css['deptime']}>到达</span>
+          <span className={css['arrtime']}>发车</span>
+          <span className={css['stoptime']}>停留时间</span>
         </div>
-        {/* <ul>{scheduleList.map((schedule, index) => schedule)}</ul> */}
+        <ul>
+          {scheduleList.map((schedule, index) => (
+            <li key={index}>
+              <div className={css['icon']}>{(index + 1).toString().padStart(2, '0')}</div>
+              <div className={css['row']}>
+                <span className={css['station']}>{schedule.station}</span>
+                <span className={css['arrtime']}>
+                  {schedule.arriveTime ? schedule.arriveTime : '始发站'}
+                </span>
+                <span className={css['deptime']}>
+                  {schedule.departTime ? schedule.departTime : '终到站'}
+                </span>
+                <span className={css['stoptime']}>
+                  {schedule.stay ? schedule.stay + '分' : '-'}
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
